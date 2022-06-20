d5943013332b32df28993aa53043ce0136673d3c
Abdul <pureexec@gmail.com>
2022-06-19 20:04:36 -0400

    finished api scripts

--- src/scripts/FindHistory.js
+++ src/scripts/FindHistory.js
@@ -3,32 +3,65 @@ import fetch from "node-fetch";
 export class WikiAccessAPI{
     constructor(url){
         this.url = url;
+        process.setMaxListeners(0);
+    }
+    parseHTML(string){
+        // remove html tags from word 
+        var string = string.replace(/<\/?[^>]+>/gi,'');
+        // find the word that encapsulates the text 
+        let scrapeWord = "&quot;"
+        // index the first instance of the word 
+        let first = string.search(scrapeWord);
+        // strip text from first index till the end 
+        string = string.slice(first+scrapeWord.length);
+        // index second instance of the word 
+        let second = string.search(scrapeWord);
+        // string text between second index and start
+        string = string.slice(second-second,second);
+        // return the substring descriptions
+        return string;
     }
     async getHistory(searchKey,searchLimit){
+        // user paramters 
         var request = `?q=${searchKey}&limit=${searchLimit}`;
+        // response of API fetch
         var response = fetch(this.url+request);
-        var events = [];
+        // 
+        // itterate over the amount of results and retrieve the necessary information
         for (let i=0; i < searchLimit;i++){
-            // get json description of response object and strip html from the json 
-            var description = await response.then(value => value.clone().json()).then(data => {
-                return data['pages'][i]['excerpt'].replace(/<\/?[^>]+>/gi, '');
-            });
-            // scrape description to obtain the essential explination
-            let scrapeWord = "&quot;" 
-            let firstScrapeIndex = description.search(scrapeWord);
-            description = description.slice(firstScrapeIndex+scrapeWord.length);
-            let secondScrapeIndex = description.search(scrapeWord);
-            description = description.slice(0,secondScrapeIndex);
-            // append all descriptions as part of an array 
-            events.push(description);
-            console.log(events)
+            // clone the response into json object for multiple use of body in promise object 
+            response.then(value => {
+                return value.clone().json();
+            })
+            // check if all forms of description in the json object are not null
+            .then(data => {
+                if((data['pages'][i]['description'] && data['pages'][i]['title']) && data['pages'][i]['excerpt'] != null) {
+                   // retrieve all forms of descriptions for the event 
+                   var eventTitle = data['pages'][i]['title'];
+                   var eventDesc = data['pages'][i]['description'];
+                   var eventExcerpt = this.parseHTML(data['pages'][i]['excerpt']);
+                   // decide between title and description and use excerpt as final resort
+                   // look for the lengthier description with a limit of 30 characters
+                   if (eventTitle.length > eventDesc.length && eventTitle.length > 30){
+                    console.log(eventTitle)
+                    //return eventTitle;
+                   }
+                   else if (eventDesc.length > eventTitle.length && eventDesc.length > 30){
+                    console.log(eventDesc)
+                    //return eventDesc;
+                   }
+                   else{
+                    console.log(eventExcerpt)
+                    //return eventExcerpt;
+                   }
+                }
+            })
         }
-        return events;
     }
 }
 
+/*
 var wiki = new WikiAccessAPI("https://en.wikipedia.org/w/rest.php/v1/search/page");
+wiki.getHistory('april 2019',2);
+*/
 
-var value = await wiki.getHistory('april 2019',1);
-
-//console.log(value);
